ee49b3551c37e9b99bc2cd1ee9b2d511b9f6a72e
Balasaiteja <balasaiteja17@gmail.com>
2024-04-22 11:13:29 +0530

    hide api key

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 submitBtn.addEventListener("click", async (e) => {
-    e.preventDefault()
-    console.log("Clicked!")
-    resultCont.innerHTML = `<img width="40px" src="/images/loading.svg" alt="">`
-    let key = "ema_live_Xw80ELBXOAETdcLKwAcierZTmtbfMjCqIqBbrqzn"
-    let email = document.getElementById("username").value
-    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
-    let res = await fetch(url)
-    let result = await res.json()
-    let str = ``
-    for (key of Object.keys(result)) {
-        if(result[key] !== "" && result[key]!== " "){
-            str = str + `<div>${key}: ${result[key]}</div>`
-        }
+  e.preventDefault();
+  console.log("Clicked!");
+  resultCont.innerHTML = `<img width="40px" src="/images/loading.svg" alt="">`;
+  let email = document.getElementById("username").value;
+  let url = `https://api.emailvalidation.io/v1/info?apikey=${config.API_KEY}&email=${email}`;
+  let res = await fetch(url);
+  let result = await res.json();
+  let str = ``;
+  for (config.API_KEY of Object.keys(result)) {
+    if (result[config.API_KEY] !== "" && result[config.API_KEY] !== " ") {
+      str = str + `<div>${config.API_KEY}: ${result[config.API_KEY]}</div>`;
     }
-    console.log(str)  
-    resultCont.innerHTML = str
-})
+  }
+  console.log(str);
+  resultCont.innerHTML = str;
+});
